55989315c2e72465d0f5d3e45c0a8e4119de9876
Freezingle <bct078011@student.ioepc.edu.np>
2023-04-21 23:51:01 +0545

    circle and parabola added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 let canvas = document.getElementById("canvas");
 let ctx= canvas.getContext("2d");
 canvas.height= 720;
@@ -11,14 +12,14 @@ let mainY=660;
 ctx.beginPath();
 function Axes()
 {
-for (let i=0; i<=canvas.height; i+=30)
+for (let i=0; i<=canvas.height; i+=10)
 {
-    ctx.lineWidth=2;
+    ctx.lineWidth=0.5;
      ctx.moveTo(0,i);
      ctx.lineTo(canvas.width,i)
 }
 
-    for (let j=0; j<=canvas.width; j+=30)
+    for (let j=0; j<=canvas.width; j+=10)
 {
      ctx.moveTo(j,0);
      ctx.lineTo(j,canvas.height);
@@ -30,7 +31,7 @@ Axes();
 function mainAxis(mainX,mainY){
     
     ctx.beginPath()
-    ctx.lineWidth=5;
+    ctx.lineWidth=3;
     //y axis
     ctx.moveTo(mainY,0)
     ctx.lineTo(mainY,canvas.height)
@@ -53,6 +54,37 @@ function OriginX(n){
     }
     mainAxis(mainX,mainY);
 }
-
+ctx.beginPath();
 mainAxis(mainX,mainY);
 
+function parabola()
+{
+    ctx.translate(mainY, mainX)
+    ctx.strokeStyle= "blue";
+    penPointX=0;
+    for (let x=0; x<700;x+=1)
+    {        
+        let y= Math.sqrt(20*x)
+        ctx.moveTo(penPointX,y);
+        ctx.lineTo(x,y);
+        penPointX=x;
+    }
+    for (let x=0; x<700;x+=1)
+    {        
+        
+        let y= -Math.sqrt(20*x)
+        ctx.moveTo(penPointX,y);
+        ctx.lineTo(x,y);
+        penPointX=x;
+    }
+
+}
+
+function circle()
+{
+    ctx.strokeStyle= "blue";
+    ctx.arc(130,50,100,0,2*Math.PI)
+}
+circle();
+parabola();
+ctx.stroke();
